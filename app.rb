a801170ff6c4c2e6e466228954cac5d81a37a2ea
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 12:02:12 -0700

    Correct Triangle class method in app.rb file.

--- app.rb
+++ app.rb
@@ -11,6 +11,6 @@ get('/triangle') do
   @side1 = params.fetch('side1').to_i
   @side2 = params.fetch('side2').to_i
   @side3 = params.fetch('side3').to_i
-  @triangle = Triangle.triangle?(@side1, @side2, @side3)
+  @triangle = Triangle.new(@side1, @side2, @side3).triangle?
   erb(:triangle)
 end
