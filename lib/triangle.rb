363ddd723504fb91a311cea40edf50f22fd0ebf0
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 10:37:09 -0700

    Add code for testing whether a triangle is an isosceles.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,14 @@ class Triangle
       "not a triangle"
     else @side2.+(@side3) <= @side1
       "not a triangle"
-      if @side1 = @side2 && @side1 = @side3
+      if @side1 == @side2 && @side1 == @side3
         "equilateral"
+      elsif @side1 = @side2 && @side1 != @side3
+        "isosceles"
+      elsif @side2 = @side3 && @side2 != @side1
+        "isosceles"
+      else @side1 = @side3 && @side1 != @side2
+        "isosceles"
       end
     end
   end
