6455cebcc2a3a88c014523f06b5451f2b33efdec
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 11:50:36 -0700

    Change integration input to integers.

--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -7,9 +7,9 @@ set(:show_exceptions, false)
 describe('the triangle path', {:type => :feature}) do
   it('processes the user entry and returns whether it is a triangle and, if so, what type') do
     visit('/')
-    fill_in('side1', :with => '5')
-    fill_in('side2', :with => '2')
-    fill_in('side3', :with => '4')
+    fill_in('side1', :with => 5)
+    fill_in('side2', :with => 2)
+    fill_in('side3', :with => 4)
     click_button('check')
     expect(page).to have_content('scalene')
   end
