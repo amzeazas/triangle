e3c796e90144f534a864ac426303ec367609b15b
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 09:30:55 -0700

    Add spec for testing whether the triangle is an equilateral.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,10 +2,14 @@ require('rspec')
 require('triangle')
 
 describe(Triangle) do
-  descibe("#triangle?") do
+  describe("#triangle?") do
     it("returns false if it is not a triangle") do
       test_triangle = Triangle.new(2, 2, 8)
       expect(test_triangle.triangle?()).to(eq(false))
+    end
+    it("processes input and returns whether it is an equilateral triangle") do
+      test_triangle = Triangle.new(2, 2, 2)
+      expect(test_triangle.triangle?()).to(eq("equilateral"))
     end
   end
 end
