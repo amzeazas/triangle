3bee015e180e18dec27f0456d3a2635f1f2af519
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-10 10:18:14 -0700

    Add spec for testing whether a triangle is an isosceles

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -11,5 +11,9 @@ describe(Triangle) do
       test_triangle = Triangle.new(2, 2, 2)
       expect(test_triangle.triangle?()).to(eq("equilateral"))
     end
+    it("processes input and returns whether it is an isosceles triangle") do
+      test_triangle = Triangle.new(2, 2, 1)
+      expect(test_triangle.triangle?()).to(eq("isosceles"))
+    end
   end
 end
